a50fa240589944147ec60838fde7044b4c444b98
filoquin <filoquin@gmail.com>
2021-11-29 19:52:23 -0300

    [FIX] Corrijo el fix de _compute_payments_widget_to_reconcile_info para que funcione correctamente   - encontraba pagos de mas

--- account_ux/models/account_move.py
+++ account_ux/models/account_move.py
@@ -105,56 +105,72 @@ class AccountMove(models.Model):
             move.invoice_outstanding_credits_debits_widget = json.dumps(False)
             move.invoice_has_outstanding = False
 
-            if move.state != 'posted' or move.payment_state != 'not_paid' or not move.is_invoice(include_receipts=True):
+            if move.state != 'posted' \
+                    or move.payment_state not in ('not_paid', 'partial') \
+                    or not move.is_invoice(include_receipts=True):
                 continue
-            pay_term_line_ids = move.line_ids.filtered(lambda line: line.account_id.user_type_id.type in ('receivable', 'payable'))
 
-            domain = [('account_id', 'in', pay_term_line_ids.mapped('account_id').ids),
-                      '|', ('move_id.state', '=', 'posted'), '&', ('move_id.state', '=', 'draft'), 
-                      ('partner_id', '=', move.commercial_partner_id.id),
-                      ('reconciled', '=', False), '|', ('amount_residual', '!=', 0.0),
-                      ('amount_residual_currency', '!=', 0.0)] # ('journal_id.post_at', '=', 'bank_rec'),
+            pay_term_lines = move.line_ids\
+                .filtered(lambda line: line.account_id.user_type_id.type in ('receivable', 'payable'))
+
+            domain = [
+                ('account_id', 'in', pay_term_lines.account_id.ids),
+                ('move_id.state', '=', 'posted'),
+                ('partner_id', '=', move.commercial_partner_id.id),
+                ('reconciled', '=', False),
+                '|', ('amount_residual', '!=', 0.0), ('amount_residual_currency', '!=', 0.0),
+            ]
+
+            payments_widget_vals = {'outstanding': True, 'content': [], 'move_id': move.id}
 
             if move.is_inbound():
-                domain.extend([('credit', '>', 0), ('debit', '=', 0)])
-                type_payment = _('Outstanding credits')
+                domain.append(('balance', '<', 0.0))
+                payments_widget_vals['title'] = _('Outstanding credits')
             else:
-                domain.extend([('credit', '=', 0), ('debit', '>', 0)])
-                type_payment = _('Outstanding debits')
-            info = {'title': '', 'outstanding': True, 'content': [], 'move_id': move.id}
-            lines = self.env['account.move.line'].search(domain)
-            currency_id = move.currency_id
-            if len(lines) != 0:
-                for line in lines:
-                    # get the outstanding residual value in invoice currency
-                    if line.currency_id and line.currency_id == move.currency_id:
-                        amount_to_show = abs(line.amount_residual_currency)
-                    else:
-                        currency = line.company_id.currency_id
-                        # as we dont add the currency information we use the rate of the invoice that is the one used by odoo compute amount_residual
-                        # INICIO CAMBIO
-                        if move.company_id.country_id == self.env.ref('base.ar'):
-                            amount_to_show = currency._convert(abs(line.amount_residual), move.currency_id, move.company_id,
-                                                            move.invoice_date or fields.Date.today())
-                        else:
-                            amount_to_show = currency._convert(abs(line.amount_residual), move.currency_id, move.company_id,
-                                                            line.date or fields.Date.today())
-                        # FIN CAMBIO
-                    if float_is_zero(amount_to_show, precision_rounding=move.currency_id.rounding):
-                        continue
-                    info['content'].append({
-                        'journal_name': line.ref or line.move_id.name,
-                        'amount': amount_to_show,
-                        'currency': currency_id.symbol,
-                        'id': line.id,
-                        'position': currency_id.position,
-                        'digits': [69, move.currency_id.decimal_places],
-                        'payment_date': fields.Date.to_string(line.date),
-                    })
-                info['title'] = type_payment
-                move.invoice_outstanding_credits_debits_widget = json.dumps(info)
-                move.invoice_has_outstanding = True
+                domain.append(('balance', '>', 0.0))
+                payments_widget_vals['title'] = _('Outstanding debits')
 
+            for line in self.env['account.move.line'].search(domain):
+
+                if line.currency_id == move.currency_id:
+                    # Same foreign currency.
+                    amount = abs(line.amount_residual_currency)
+                else:
+                    # Different foreign currencies.
+                    if move.company_id.country_id == self.env.ref('base.ar'):
+                        amount = move.company_currency_id._convert(
+                            abs(line.amount_residual),
+                            move.currency_id,
+                            move.company_id,
+                            move.invoice_date,
+                        )
+                    else:
+                        amount = move.company_currency_id._convert(
+                            abs(line.amount_residual),
+                            move.currency_id,
+                            move.company_id,
+                            line.date,
+                        )
+
+                if move.currency_id.is_zero(amount):
+                    continue
+
+                payments_widget_vals['content'].append({
+                    'journal_name': line.ref or line.move_id.name,
+                    'amount': amount,
+                    'currency': move.currency_id.symbol,
+                    'id': line.id,
+                    'move_id': line.move_id.id,
+                    'position': move.currency_id.position,
+                    'digits': [69, move.currency_id.decimal_places],
+                    'payment_date': fields.Date.to_string(line.date),
+                })
+
+            if not payments_widget_vals['content']:
+                continue
+
+            move.invoice_outstanding_credits_debits_widget = json.dumps(payments_widget_vals)
+            move.invoice_has_outstanding = True
 
     def _get_reconciled_info_JSON_values(self):
         self.ensure_one()
