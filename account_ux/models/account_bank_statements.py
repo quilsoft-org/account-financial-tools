3a1bedfcb0df0ab86f54177093479dba9c36d57b
duffgnr14 <gabi.141312@gmail.com>
2023-06-06 14:44:59 -0300

    [upd]campo active en account.move

--- account_ux/models/account_bank_statements.py
+++ account_ux/models/account_bank_statements.py
@@ -2,12 +2,13 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models
+from odoo import models, fields
 
 
 class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement'
 
+    active = fields.Boolean(default=True)
     def cancel_all_lines(self):
         for rec in self:
             rec.line_ids.filtered('journal_entry_ids').button_cancel_reconciliation()
